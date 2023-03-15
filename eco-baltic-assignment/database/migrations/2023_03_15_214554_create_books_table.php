3daf95934b1e4726088f7155c2c9fb8403eeffe3
mockery <tripezzshow@gmail.com>
2023-03-16 00:35:07 +0200

    added factories and test routes

--- eco-baltic-assignment/database/migrations/2023_03_15_214554_create_books_table.php
+++ eco-baltic-assignment/database/migrations/2023_03_15_214554_create_books_table.php
@@ -16,7 +16,7 @@ class CreateBooksTable extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('description');
+            $table->longText('description');
             $table->timestamps();
         });
     }
--- eco-baltic-assignment/routes/web.php
+++ eco-baltic-assignment/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Author;
+use App\Models\Book;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/books', function () {
+    return Book::all();
+});
+
+Route::get('/authors', function () {
+    return Author::all();
 });
